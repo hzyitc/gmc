fdfcd610cc007fdcac953d236b9a4dfbeb58e77b
hzy <h1260071766@126.com>
2022-04-16 20:42:48 +0800

    Refactor

--- main.go
+++ main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
-	"flag"
+	"errors"
+	"net"
+	"os"
 	"strconv"
 
 	"github.com/hzyitc/mnh/log"
 )
 
+var version = "v0.0.0"
+
+func usage() {
+	print(os.Args[0] + " (" + version + ")\n")
+	print("Usage:\n")
+	print("  " + os.Args[0] + " tcp <listen port> <mnh query url>\n")
+}
+
 func main() {
-	flag.Parse()
-	args := flag.Args()
+	args := os.Args[1:]
 
-	port, err := strconv.Atoi(args[0])
+	if len(args) < 3 || len(args) > 3 {
+		usage()
+		return
+	}
+
+	port, err := strconv.Atoi(args[1])
+	if err != nil {
+		log.Error("Parse port error:", err.Error())
+		return
+	}
+
+	service := ""
+	connectFunc := func(local net.Conn) (net.Conn, error) {
+		log.Info("new connection", local.RemoteAddr().String())
+
+		if service != "" {
+			remote, err := net.Dial("tcp", service)
+			if err == nil {
+				return remote, nil
+			}
+		}
+
+		log.Info("Querying...")
+		addr, err := mnhv1_query(args[2])
+		if err != nil {
+			return nil, err
+		}
+		log.Info("Result: " + addr)
+
+		service = addr
+		return net.Dial("tcp", service)
+	}
+
+	switch args[0] {
+	case "tcp":
+		err = TCPProxy(port, connectFunc)
+	default:
+		err = errors.New("Unknown mode: " + args[0])
+	}
 	if err != nil {
 		log.Error(err.Error())
 		return
 	}
-
-	s, err := NewProxy(port, args[1])
-	if err != nil {
-		log.Error(err.Error())
-		return
-	}
-
-	<-s.ClosedChan()
 }
--- proxy.go
+++ proxy.go
@@ -9,151 +9,66 @@ import (
 	"github.com/hzyitc/mnh/log"
 )
 
-type Interface interface {
-	ClosedChan() <-chan struct{}
-	Close() error
-}
+type ConnectFunc func(local net.Conn) (net.Conn, error)
 
-type proxy struct {
-	port int
-	url  string
-
-	service string
-
-	worker      *sync.WaitGroup
-	closingChan chan struct{}
-	closedChan  chan struct{}
-
-	listener net.Listener
-}
-
-func (s *proxy) server_tryConnect() (net.Conn, error) {
-	if s.service != "" {
-		c, err := net.Dial("tcp", s.service)
-		if err == nil {
-			return c, nil
-		}
-	}
-
-	addr, err := mnhv1_query(s.url)
-	if err != nil {
-		return nil, err
-	}
-
-	s.service = addr
-
-	return net.Dial("tcp", s.service)
-}
-
-func (s *proxy) server_handle(conn net.Conn) {
-	s.worker.Add(+1)
-	defer s.worker.Done()
-
-	defer conn.Close()
-
-	log.Info("new connection", conn.RemoteAddr().String())
-
-	c, err := s.server_tryConnect()
-	if err != nil {
-		log.Error(conn.RemoteAddr().String(), err.Error())
-		return
-	}
-	defer c.Close()
-
-	prefix := conn.RemoteAddr().String() + "->" + c.RemoteAddr().String() + ":"
-
-	log.Info(prefix, "connected")
-	defer log.Info(prefix, "closed")
-
-	closing := make(chan int)
+func DuplexForward(c1, c2 net.Conn) {
+	worker := sync.WaitGroup{}
+	worker.Add(+2)
 
 	go func() {
-		io.Copy(conn, c)
-		conn.Close()
-		closing <- 1
+		defer worker.Done()
+
+		io.Copy(c1, c2)
+		c1.Close()
 	}()
 
 	go func() {
-		io.Copy(c, conn)
-		c.Close()
-		closing <- 1
+		defer worker.Done()
+
+		io.Copy(c2, c1)
+		c2.Close()
 	}()
 
-	running := 2
-	for {
-		select {
-		case <-s.closingChan:
-			return
-		case <-closing:
-			running--
-			if running == 0 {
-				return
-			}
-		}
-	}
+	worker.Wait()
 }
 
-func (s *proxy) server_main() {
-	s.worker.Add(+1)
-	defer s.worker.Done()
+func Proxy(listener net.Listener, connectFunc ConnectFunc) error {
+	worker := sync.WaitGroup{}
 
 	for {
-		conn, err := s.listener.Accept()
+		local, err := listener.Accept()
 		if err != nil {
-			select {
-			case <-s.closingChan:
-				return
-			default:
-				log.Error("server_main error", err.Error())
-				continue
-			}
+			return err
 		}
-		go s.server_handle(conn)
+
+		remote, err := connectFunc(local)
+		if err != nil {
+			local.Close()
+			return err
+		}
+
+		if remote != nil {
+			worker.Add(+1)
+			go func() {
+				defer worker.Done()
+
+				prefix := local.RemoteAddr().String() + "->" + remote.RemoteAddr().String() + ":"
+				log.Info(prefix, "connected")
+				defer log.Info(prefix, "closed")
+
+				DuplexForward(local, remote)
+			}()
+		}
 	}
 }
 
-func NewProxy(port int, url string) (Interface, error) {
+func TCPProxy(port int, connectFunc ConnectFunc) error {
 	local := "0.0.0.0:" + strconv.Itoa(port)
 	listener, err := net.Listen("tcp", local)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	s := &proxy{
-		port,
-		url,
-
-		"",
-
-		new(sync.WaitGroup),
-		make(chan struct{}),
-		make(chan struct{}),
-
-		listener,
-	}
-
-	go s.server_main()
-
-	return s, nil
-}
-
-func (s *proxy) ClosedChan() <-chan struct{} {
-	return s.closedChan
-}
-
-func (s *proxy) Close() error {
-	select {
-	case <-s.closingChan:
-		return nil
-	default:
-		break
-	}
-	close(s.closingChan)
-
-	err := s.listener.Close()
-
-	s.worker.Wait()
-
-	close(s.closedChan)
-	return err
+	defer listener.Close()
+	return Proxy(listener, connectFunc)
 }
